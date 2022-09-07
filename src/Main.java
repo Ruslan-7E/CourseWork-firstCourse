fe1b652e541a05175a0a769dc2b32e46baffa591
Ruslan Mukhametshin <ruslanow597@gmail.com>
2022-09-07 23:22:22 +0300

    How to realize methods?

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,19 @@ public class Main {
     private static final Employee[] employees = new Employee[10];
 
     public static void main(String[] args) {
-        // Надо ли заполнять произвольные данные всех 10-ти сотрудников.
+        // Надо ли заполнять произвольные данные всех 10-ти сотрудников. Заполнил.
         Employee employees1 = new Employee("Ivanov Ivan Ivanovich", 1, 102_000);
-        Employee employees2 = new Employee("Petrov Peter Petrovich", 4, 111_000);
+        Employee employees2 = new Employee("Petrov Peter Petrovich", 1, 111_000);
         Employee employees3 = new Employee("Sidorov Vasiliy Sidorovich", 2, 120_000);
-        Employee employees4 = new Employee("Musk Ilon Batkovich", 3, 156_000);
+        Employee employees4 = new Employee("Mukhametshin Ruslan Ildarovich", 2, 136_000);
+        Employee employees5 = new Employee("Vasiliyev Vasiliy Vasiliyevich", 3, 150_000);
+        Employee employees6 = new Employee("Bezos Jeffrey Amazonovich", 3, 156_000);
+        Employee employees7 = new Employee("Pupkin Fedor Anreevich", 4, 166_000);
+        Employee employees8 = new Employee("Guido van Rossum", 4, 177_000);
+        Employee employees9 = new Employee("Gates Bill Microsoftovich", 5, 196_000);
+        Employee employees10 = new Employee("Musk Ilon Batkovich", 5, 199_000);
 
         System.out.println(employees1);
-
-        Employee employees5 = addEmployeeToEmployees("Name Surname Patronymic", 5, 160_000, 1);
-
-        System.out.println(employees5);
 
         findMinSalary();
 
